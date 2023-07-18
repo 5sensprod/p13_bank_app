721377aa007c3d7f39d8fa7210aba4d9d8c53990
chris <5sensprod@gmail.com>
2023-07-18 23:28:37 +0200

    Ajout privateRoute for user

---
+++ src/routes/PrivateRoute.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { useSelector } from 'react-redux'
+import { Navigate } from 'react-router-dom'
+
+const PrivateRoute = ({ children }) => {
+  const { isLoggedIn } = useSelector((state) => state.user)
+
+  if (!isLoggedIn) {
+    return <Navigate to="/sign-in" />
+  }
+
+  return children
+}
+
+export default PrivateRoute
--- src/routes/Router.js
+++ src/routes/Router.js
@@ -3,13 +3,21 @@ import { Routes, Route } from 'react-router-dom'
 import Home from '../pages/Home'
 import Login from '../pages/Login'
 import User from '../pages/User'
+import PrivateRoute from './PrivateRoute'
 
 const AppRouter = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/sign-in" element={<Login />} />
-      <Route path="/user" element={<User />} />
+      <Route
+        path="/user"
+        element={
+          <PrivateRoute>
+            <User />
+          </PrivateRoute>
+        }
+      />
     </Routes>
   )
 }
