dec2d5e16347d33243a4fb8858a3eafe087a11cd
chris <5sensprod@gmail.com>
2023-07-28 19:36:59 +0200

    finalisation swagger

--- src/actions/accountActions.js
+++ src/actions/accountActions.js
@@ -20,49 +20,27 @@ export const fetchAccountsFailure = (error) => ({
 export const fetchUserAccounts = (userId) => async (dispatch) => {
   dispatch(fetchAccountsRequest())
 
-  // Récupére les comptes depuis localStorage d'abord
-  const cachedAccounts = localStorage.getItem('userAccounts')
-
-  if (cachedAccounts) {
-    try {
-      const parsedData = JSON.parse(cachedAccounts)
-      if (parsedData.body && Array.isArray(parsedData.body)) {
-        dispatch(fetchAccountsSuccess(parsedData.body))
-      } else {
-        console.error('Invalid data structure in localStorage:', cachedAccounts)
-      }
-    } catch (e) {
-      console.error('Error parsing data from localStorage:', e)
-    }
-  } else {
-    // Si aucun compte n'est en cache, ils sont récupérés depuis le serveur
-    try {
-      const token = localStorage.getItem('token')
-      const response = await fetch(
-        `http://localhost:3001/api/v1/user/${userId}/accounts`,
-        {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
+  try {
+    const token = localStorage.getItem('token')
+    const response = await fetch(
+      `http://localhost:3001/api/v1/user/${userId}/accounts`,
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
         },
-      )
+      },
+    )
 
-      if (response.ok) {
-        const data = await response.json()
+    if (response.ok) {
+      const data = await response.json()
 
-        // Stocke l'intégralité de la réponse dans localStorage pour les utilisations futures
-        localStorage.setItem('userAccounts', JSON.stringify(data))
-
-        // Utilisez data.body pour dispatcher vers le store Redux
-        dispatch(fetchAccountsSuccess(data.body))
-      } else {
-        throw new Error(
-          'Erreur de récupération des comptes : ' + response.status,
-        )
-      }
-    } catch (error) {
-      console.error(error)
-      dispatch(fetchAccountsFailure(error))
+      // Utilise data.body pour dispatcher vers le store Redux
+      dispatch(fetchAccountsSuccess(data.body))
+    } else {
+      throw new Error('Erreur de récupération des comptes : ' + response.status)
     }
+  } catch (error) {
+    console.error(error)
+    dispatch(fetchAccountsFailure(error))
   }
 }
