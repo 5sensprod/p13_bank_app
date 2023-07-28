0d133471db1619fa1409e5214ee7e8f18bf01541
chris <5sensprod@gmail.com>
2023-07-28 11:52:50 +0200

    ajout favicon + correction protypes

--- src/data/features.js
+++ src/data/features.js
@@ -2,19 +2,25 @@ export const featuresData = [
   {
     id: 1,
     imgSrc: '/assets/icon-chat.png',
+    imgAlt: 'Chat Icon',
     title: 'You are our #1 priority',
+    description: '',
     text: 'Need to talk to a representative? You can get in touch through our 24/7 chat or through a phone call in less than 5 minutes.',
   },
   {
     id: 2,
     imgSrc: '/assets/icon-money.png',
+    imgAlt: 'Money Icon',
     title: 'More savings means higher rates',
+    description: '',
     text: 'The more you save with us, the higher your interest rate will be!',
   },
   {
     id: 3,
     imgSrc: '/assets/icon-security.png',
+    imgAlt: 'Security Icon',
     title: 'Security you can trust',
+    description: '',
     text: 'We use top of the line encryption to make sure your data and money is always safe.',
   },
 ]
